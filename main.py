d2ae44f960d59eef40943fa9dd85cbc239497158
arsenpaw <arsenpaw7@gmail.com>
2023-11-22 14:00:45 +0200

    add ppython first lab code

---
+++ main.py
@@ -0,0 +1,12 @@
+holiday = "Christmas"
+print(holiday)
+new_year = "new year"
+print(holiday , new_year)
+is_adult = True
+is_teen = False
+print(is_adult)
+print(is_teen)
+
+is_windy = True
+is_calm = True
+print(is_windy or is_calm)
